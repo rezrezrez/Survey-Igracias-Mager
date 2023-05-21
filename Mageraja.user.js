17a8588faf4ff08e6f904b0c7c26eaa09423a8b0
rezrezrez <rezassyah9@gmail.com>
2023-05-21 16:08:26 +0700

    Add files via upload

---
+++ Mageraja.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Mager isi aja
+// @namespace    http://tampermonkey.net/
+// @version       Alphaedition
+// @description  Auto isi survey dan tekan tombol simpan secara otomatis
+// @author       hehe.
+// @match        https://igracias.telkomuniversity.ac.id/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var multi = 'Puas';
+    var option = 'Ya';
+    var inputText = '-';
+
+    $('.answerlist1:contains('+multi+'), .answerlist1:contains('+option+')').each(function(){
+        $(this).parent().each(function(){
+            $(this).find('.answerlist2').children().click()
+        })
+    });
+
+    // Mengisi semua elemen <textarea> dengan tanda "-"
+    $('textarea').val(inputText);
+
+    // Menekan tombol "Simpan" secara otomatis
+    $('input[name="button"]').click();
+})();
